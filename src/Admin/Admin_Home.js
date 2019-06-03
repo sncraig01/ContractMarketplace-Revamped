799e64a5fc7ed85cf1ad9b373bfc6ad97fc171ea
Leandra Klein <lok2ea@virginia.edu>
2019-06-03 17:19:49 -0400

    fixed admin navbar links

--- src/Admin/Admin_Home.js
+++ src/Admin/Admin_Home.js
@@ -1,41 +1,32 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import Paper from '@material-ui/core/Paper';
-import List from '@material-ui/core/List';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import './Admin_Home.css';
-import Admin_NavBar from './Admin_NavBar';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import Paper from "@material-ui/core/Paper";
+import List from "@material-ui/core/List";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import "./Admin_Home.css";
+import Admin_NavBar from "./Admin_NavBar";
 
-
-class Admin_Home extends React.Component 
-{
-   
-    render()
-    {
-        return (
-            <div className= 'Admin-whole'>
-                <Admin_NavBar/>
-                <div className = 'Admin-Cards'>
-                    <Card className='Admin-header'> 
-                        <div>
-                            <b>
-                                Profile: 
-                            </b>
-                        </div>
-                        <CardContent className="Admin-Content">
-                            Welcome Admin:
-                        <div>
-                        
-                        </div>
-                        </CardContent>
-                    </Card>   
-                </div>
+class Admin_Home extends React.Component {
+  render() {
+    return (
+      <div className="Admin-whole">
+        <Admin_NavBar title={"Home"} />
+        <div className="Admin-Cards">
+          <Card className="Admin-header">
+            <div>
+              <b>Profile:</b>
             </div>
-          );
-    }
+            <CardContent className="Admin-Content">
+              Welcome Admin:
+              <div />
+            </CardContent>
+          </Card>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Admin_Home;
-
--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import StudentSignup from "./StudentSignup.js";
-import CompanySignup from "./CompanySignup.js";
+// import StudentSignup from "./StudentSignup.js";
+// import CompanySignup from "./CompanySignup.js";
 import "./App.css";
 import Routes from "./Routes";
-import Company_Home from "./Company/Company_Home";
-import Authentication from "./Authentication.js";
-import Landing from "./Landing.js"
+// import Company_Home from "./Company/Company_Home";
+// import Authentication from "./Authentication.js";
+// import Landing from "./Landing.js";
+// import Admin_NavBar from "./Admin/Admin_NavBar";
 
 function App() {
   return (
