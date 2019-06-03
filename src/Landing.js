d60f94d86924aac751fdf073b332e55960acbf0f
Sarah  Craig <sarahcraig@d-172-25-231-99.dhcp.virginia.edu>
2019-06-03 14:41:34 -0400

    landing page functionality

--- src/Landing.js
+++ src/Landing.js
@@ -3,8 +3,6 @@ import './App.css';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 
-
-
 const useStyles = makeStyles(theme => ({
     button: {
       margin: theme.spacing(1),
@@ -15,26 +13,48 @@ const useStyles = makeStyles(theme => ({
   }));
 
 class Landing extends React.Component{
+    constructor(props) {
+        super(props);
+    }
+    
+    signInClicked = e => {
+        // Redirects to profile page
+        this.props.history.push("/signin");
+    };
+    
+    signUpStudClicked = e => {
+        // Redirects to profile page
+        this.props.history.push("/signupstudent");
+    };
 
-
-
+    signUpCompClicked = e => {
+        // Redirects to profile page
+        this.props.history.push("/signupcompany");
+    };
 
     render() {
-        const classes = useStyles();
+        const classes = useStyles;
 
         return (
             <div className="App">
                 <h1> Welcome to RevTek Contract Marketplace! </h1>
+                <p> Select how you would like to sign in</p>
+                <div> 
+                    <Button variant="contained" className={classes.button} onClick={ ()=> this.signInClicked()}>
+                        Sign In
+                    </Button>
+                </div>
+                <br/>
                 <div>
-                <Button variant="contained" className={classes.button}>
-                    Sign In
-                </Button>
-                <Button variant="contained" color="primary" className={classes.button}>
-                    Sign Up as Student
-                </Button>
-                <Button variant="contained" color="secondary" className={classes.button}>
-                    Sign Up as Company
-                </Button>
+                    <Button variant="contained" color="primary" className={classes.button} onClick={()=> this.signUpStudClicked()} >
+                        Sign Up as Student
+                    </Button>
+                </div>
+                <br/>
+                <div>
+                    <Button variant="contained" color="secondary" className={classes.button} onClick={()=> this.signUpCompClicked()} >
+                        Sign Up as Company
+                    </Button>
                 </div>
             </div>
           );
